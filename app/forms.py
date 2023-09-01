2cfc9960e80e3634d30fa3c7612e1067cf6b2df4
code9633 <shashikaudara5566@gmail.com>
2023-09-02 00:10:03 +0530

    adding style  to form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,25 @@
 from django import forms
 from app.models import MyModel
 
-class MyForm(forms.ModelForm):
+class MyForm(forms.ModelForm): 
+    
     class Meta:
         model = MyModel
-        fields = ['field1', 'field2', 'field3']
+        fields = ['field1','field2', 'field3']
+        
+        widgets = {
+            'field1': forms.TextInput(attrs={
+                'class' : 'form-control'
+            }),
+            
+            'field2' : forms.TextInput(attrs ={
+                'class' : 'form-control',
+                'label' : 'Field 2 (Integer Only)'
+            }),
+            
+            'field3': forms.CheckboxInput(attrs = {
+                'class' : 'form-check-input'
+            })
+        }
+        
 
